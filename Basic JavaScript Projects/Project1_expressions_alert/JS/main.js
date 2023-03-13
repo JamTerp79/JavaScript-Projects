ee3a4c93874eeb886b7c46447ec875943309f6c6
JamTerp79 <terpening.james@gmail.com>
2023-03-13 18:43:56 -0500

    Starting out simple

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,2 +1,2 @@
 var Restaurant = "The Wagon Wheel", Owner = " Clint Eastwood ", Manager = " John Wayne ", Menu = "Steak....and Snowflakes";  // Naming multiple variables //  
-    window.alert((Owner) + (Manager));  // Using the variables we created. //
+    window.alert((Owner) + (Manager) + (3 + 3));  // Using the variables we created. //
--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -1,2 +1,5 @@
-var pass = ("move on");
-document.getElementById (pass)
+function myFunction()  { // The Function //
+    var sentence = "I am learning";// Creating a sentence variable //
+    sentence += " a lot from this book!";// Concatenating the strings //
+    document.getElementById("Concatenate").innerHTML = sentence;// Runs the function when id is called in html //
+}
