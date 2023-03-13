23ba5cb630034c69922a9f06d07109933e617717
JamTerp79 <terpening.james@gmail.com>
2023-03-13 16:58:41 -0500

    Adding comments to js

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,3 +1,2 @@
-var Restaurant = "The Wagon Wheel", Owner = "Clint Eastwood", Manager = "John Wayne", Menu = "Steak....and Snowflakes";    
-    document.write(Menu);
-
+var Restaurant = "The Wagon Wheel", Owner = "Clint Eastwood", Manager = "John Wayne", Menu = "Steak....and Snowflakes";  // Naming multiple variables //  
+    document.write(3 + 3);  // Using NONE of the variables we created....BUT....We do have an expression. //
