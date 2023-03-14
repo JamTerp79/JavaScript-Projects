81ef77e32c2f9196d563c95d450212f6e49f4b65
JamTerp79 <terpening.james@gmail.com>
2023-03-13 21:49:44 -0500

    math functions

--- Basic JavaScript Projects/Project3_math_operators/JS/main.js
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -1,4 +1,54 @@
-function theHandler()   {
-    var Substraction = 12 - 9;
-    document.getElementById("Math").innerHTML ="12 - 9 = " + Substraction;
-}
+ function addNumbers()  {
+    // add function - I could not sort out the input options, so I kept absolute values//
+    let num1 = 5; //First number//
+    let num2 = 5; //Second number//
+    let result = num1 + num2; //Executes operator and stores result//
+    document.getElementById("result").innerHTML = (result); //Displays result//
+ }
+
+ function subtractNumbers()  {
+    let num1 = 12;
+    let num2 = 5;
+    let result = num1 - num2;
+    document.getElementById("result").innerHTML = (result);
+ }
+
+ function multiplyNumbers()  {
+    let num1 = 3;
+    let num2 = 6;
+    let result = num1 * num2;
+    document.getElementById("result").innerHTML = (result);
+ }
+
+ function divideNumbers()  {
+    let num1 = 12;
+    let num2 = 4;
+    let result = num1 / num2;
+    document.getElementById("result").innerHTML = (result);
+ }
+
+ function modulusNumbers()  {
+    let num1 = 12;
+    let num2 = 5;
+    let result = num1 % num2;
+    document.getElementById("result").innerHTML = (result);
+ }
+
+ function incrementNumber() {
+    num1 = 12;
+    num2 = ++num1;
+    result = num2;
+    document.getElementById("result").innerHTML = (result);
+ }
+
+ function decrementNumber() {
+    num1 = 12;
+    num2 = --num1;
+    result = num2;
+    document.getElementById("result").innerHTML = (result);
+ }
+
+ function randomNumber()    {
+    result = (Math.random() * 100);
+    document.getElementById("result").innerHTML = (result);
+ }
