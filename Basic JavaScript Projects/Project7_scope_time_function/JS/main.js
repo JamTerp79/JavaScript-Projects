73c3ce0f488d2792696731e7e35f2b5eb82d2db3
JamTerp79 <terpening.james@gmail.com>
2023-03-21 13:19:20 -0500

    correcting assignment

--- Basic JavaScript Projects/Project7_scope_time_function/JS/main.js
+++ Basic JavaScript Projects/Project7_scope_time_function/JS/main.js
@@ -1,15 +1,26 @@
+// Declare a global variable
+var greeting = "Hello!";
+
 function Time_function() {
-    var Time = new Date().getHours(); // Get the current hour //
-    var Reply; // Create a variable to hold the reply //
-    if (Time < 12 == Time > 0) { // If time is between 0 and 12 //
-        Reply = "It is morning time!"; // Reply with this //
+    // Declare a local variable
+    var Time = new Date().getHours();
+    var Reply;
+    if (Time < 12 && Time > 0) { // Fix the if statement to use && instead of ==
+        Reply = "It is morning time!";
     }
-    else if (Time >= 12 == Time < 18) {  // If time is between 12 and 18 //
-        Reply = "It is the afternoon."; // Reply with this //
+    else if (Time >= 12 && Time < 18) {
+        Reply = "It is the afternoon.";
     }
-    else { // If time is between 18 and 0 //
-        Reply = "It is evening time."; // Reply with this //
+    else {
+        Reply = "It is evening time.";
     }
-    document.getElementById("Time_of_day").innerHTML = Reply; // Display the reply //
+    document.getElementById("Time_of_day").innerHTML = Reply;
 }
+
+function Greeting_function(name) {
+    console.log(greeting + " " + name); // Call a non-existent variable to cause an error
+}
+
+Greeting_function("John"); // Call the function to trigger the error
+
     
