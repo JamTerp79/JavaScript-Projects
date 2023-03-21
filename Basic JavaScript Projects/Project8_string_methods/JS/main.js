1419e8b0bd53439e40f0c5642caebb42fcb9fa5e
JamTerp79 <terpening.james@gmail.com>
2023-03-21 15:48:18 -0500

    This was fun...

--- Basic JavaScript Projects/Project8_string_methods/JS/main.js
+++ Basic JavaScript Projects/Project8_string_methods/JS/main.js
@@ -1,5 +1,20 @@
+var velocity = 24
+var exactly = 23.945613
+var part_1 = "We are ";
+var part_2 = "the knights ";
+var part_3 = "who say ";
+var part_4 = "Ni!";
+var whole_sentence = part_1.concat(part_2, part_3, part_4);
+function full_Sentence()    {
+    document.getElementById("Concatenate").innerHTML = whole_sentence;
+}
 function slice_Method() {
-    var Sentence = "By the power of Greyskull, I have the power!";
-    var Section = Sentence.slice(16,25);
+    var Section = whole_sentence.slice(11,18);
     document.getElementById("Slice").innerHTML = Section;
+}
+function string_Method() {
+    document.getElementById("Numbers_to_string").innerHTML = velocity.toString();
+}
+function precision_Method() {
+    document.getElementById("Precision").innerHTML = exactly.toPrecision(3);
 }
