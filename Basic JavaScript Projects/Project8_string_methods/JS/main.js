56d8732c2426dea05e8eff2f7fe4abba3be9cf5a
JamTerp79 <terpening.james@gmail.com>
2023-03-21 13:51:37 -0500

    Back to project 7 again...

--- Basic JavaScript Projects/Project8_string_methods/JS/main.js
+++ Basic JavaScript Projects/Project8_string_methods/JS/main.js
@@ -1,8 +1,5 @@
-function full_Sentence() {
-    var part_1 = "We are ";
-    var part_2 = "the knights ";
-    var part_3 = "who say ";
-    var part_4 = "Ni!";
-    var whole_sentence = part_1.concat(part_2, part_3, part_4);
-    document.getElementById("Concatenate").innerHTML = whole_sentence;
+function slice_Method() {
+    var Sentence = "By the power of Greyskull, I have the power!";
+    var Section = Sentence.slice(16,25);
+    document.getElementById("Slice").innerHTML = Section;
 }
