885c4eaf39acf73997b3da250efe263e616b094f
JamTerp79 <terpening.james@gmail.com>
2023-03-21 12:50:45 -0500

    time is on my side..

--- Basic JavaScript Projects/Project7_scope_time_function/JS/main.js
+++ Basic JavaScript Projects/Project7_scope_time_function/JS/main.js
@@ -1,11 +1,15 @@
-function Quest_Function() {
-var Quest = document.getElementById("Quest").value;
-  if (Quest === "To search for the Holy Grail") {
-    Quest = "You may pass!";
-  }
-  else {
-    Quest = "ARGH! You shall not pass!";
-  }
-  document.getElementById("What_is_your_quest").innerHTML = Quest;
-} 
+function Time_function() {
+    var Time = new Date().getHours(); // Get the current hour //
+    var Reply; // Create a variable to hold the reply //
+    if (Time < 12 == Time > 0) { // If time is between 0 and 12 //
+        Reply = "It is morning time!"; // Reply with this //
+    }
+    else if (Time >= 12 == Time < 18) {  // If time is between 12 and 18 //
+        Reply = "It is the afternoon."; // Reply with this //
+    }
+    else { // If time is between 18 and 0 //
+        Reply = "It is evening time."; // Reply with this //
+    }
+    document.getElementById("Time_of_day").innerHTML = Reply; // Display the reply //
+}
     
