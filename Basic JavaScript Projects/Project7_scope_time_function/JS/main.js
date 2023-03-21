cb24d73cefe24726b25474a03ffa9577e845272a
JamTerp79 <terpening.james@gmail.com>
2023-03-21 12:43:07 -0500

    Lesson in ensuring I am in the correct files LOL

--- Basic JavaScript Projects/Project7_scope_time_function/JS/main.js
+++ Basic JavaScript Projects/Project7_scope_time_function/JS/main.js
@@ -1,15 +1,11 @@
-function myFunction() {
-    var hour = new Date().getHours();
-    var greeting;
-  
-    if (hour < 12) {
-      greeting = "Good morning!";
-    } else if (hour < 18) {
-      greeting = "Good afternoon!";
-    } else {
-      greeting = "Good evening!";
-    }
-  
-    document.getElementById("my-paragraph").innerHTML = greeting;
+function Quest_Function() {
+var Quest = document.getElementById("Quest").value;
+  if (Quest === "To search for the Holy Grail") {
+    Quest = "You may pass!";
   }
-  
+  else {
+    Quest = "ARGH! You shall not pass!";
+  }
+  document.getElementById("What_is_your_quest").innerHTML = Quest;
+} 
+    
